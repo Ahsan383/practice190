e84c8fc4ed5780b0a36993461c52414a4e0364e5
ahsan383 <muhammadahsank74@gnail.com>
2024-02-26 21:35:15 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var message = "Ahsan khan";
+console.log(message);
+message = "123";
+var isAdmin = true;
+console.log(message);
+// E.g . good day, Ahmed! 
+var personName = "Ahmed";
+console.log("Good Day," + personName + "!");
---
+++ index.ts
@@ -0,0 +1,8 @@
+let message:string="Ahsan khan"
+console.log(message);
+message="123"
+let isAdmin=true
+console.log(message);
+// E.g . good day, Ahmed! 
+let personName:string="Ahmed"
+console.log("Good Day,"+personName+"!");
